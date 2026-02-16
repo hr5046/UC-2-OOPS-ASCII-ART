29155d8459b81a817af3eab27bd10d262cc1d1ba
Harshvardhan Singh Rawat <hr5046@srmist.edu.in>
2026-02-16 08:36:44 +0530

    Add files via upload

---
+++ OOPS_ASCII.java
@@ -0,0 +1,14 @@
+class OOPS_ASCII {
+    public static void main(String[] args) {
+
+        System.out.println(  " ***** " + " " + " ***** " + " " +" ****** " + " " +"******* ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** ** " + " " + " ****");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** ** " + " " + " ** ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ****** " + " " +"******* ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** " + " " + "         **");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** " + " " + "      ** **");
+        System.out.println(" ***** " + " " + " ***** " + " " + " ** " + " " + "   ******* ");
+
+
+    }
+}
